18cd1fc48aaaa15184719f80236439e2b4b06adc
Felix <felix-schindler@outlook.com>
2024-02-21 03:05:27 +0800

    Revert ":warning: Add `URLCache`"
    This reverts commit 7130b926aa2a8f729f1f69e1e3bf4c1774e8fbf8.

--- Sources/SwiftHttp/UrlSessionHttpClient.swift
+++ Sources/SwiftHttp/UrlSessionHttpClient.swift
@@ -10,12 +10,12 @@ import Logging
 
 /// Default URLSession based implementation of the HttpClient protocol
 public struct UrlSessionHttpClient: HttpClient {
-    
+
     private let loggerLabel = "com.binarybirds.swift-http"
-    
+
     let session: URLSession
     let logger: Logger
-    
+
     ///
     /// Initializes an instance of `HTTPClient` with the given session and logging level.
     ///
@@ -30,12 +30,28 @@ public struct UrlSessionHttpClient: HttpClient {
     ) {
         var logger = Logger(label: loggerLabel)
         logger.logLevel = logLevel
-        
-        URLCache.setup()
+
         self.session = session
         self.logger = logger
     }
-    
+
+    ///
+    /// Initialize a new client object
+    ///
+    /// - Parameter session: The URLSession instance, default: `.shared`
+    /// - Parameter log: Is logging enabled for the client
+    ///
+    /// - Returns: An instance of `HTTPClient`.
+    ///
+    @available(*, deprecated, message: "Use init(session:logLevel:) instead.")
+    public init(session: URLSession = .shared, log: Bool = false) {
+        var logger = Logger(label: loggerLabel)
+        logger.logLevel = log ? .info : .critical
+
+        self.session = session
+        self.logger = logger
+    }
+
     ///
     /// Performs a data task (in memory) HTTP request
     ///
@@ -47,44 +63,22 @@ public struct UrlSessionHttpClient: HttpClient {
     ///
     public func dataTask(_ req: HttpRequest) async throws -> HttpResponse {
         let urlRequest = req.urlRequest
-        
-        // FIXME: Without this `if` it also returns cache for DELETE requests, even tho they should be saved because of the if below (:75)
-        if (req.method == .get) {
-            if let cachedResponse = URLCache.shared.cachedResponse(for: urlRequest) {
-                // Check if the response is already cached
-                do {
-                    let httpResponse = try HttpRawResponse((cachedResponse.data, cachedResponse.response))
-                    logger.debug("Cache found for \(req.method.rawValue) \(req.url)")
-                    return httpResponse
-                } catch {
-                    logger.error("Failed to create response from cache")
-                }
-            }
-        }
-        
         logger.info(.init(stringLiteral: urlRequest.curlString))
-        
+
         // Type: (Data, URLResponse)
         let res = try await session.data(for: urlRequest)
-        
+
         do {
             let rawResponse = try HttpRawResponse(res)
-            // logger.trace(.init(stringLiteral: rawResponse.traceLogValue))
+            logger.trace(.init(stringLiteral: rawResponse.traceLogValue))
             logger.debug(.init(stringLiteral: res.0.logValue))
-            
-            if (req.method == .get) {
-                // Store the response in the cache
-                URLCache.shared.storeCachedResponse(CachedURLResponse(response: res.1, data: res.0), for: urlRequest)
-                logger.debug("Cache stored for \(req.method.rawValue) \(req.url)")
-            }
-            
             return rawResponse
         } catch {
             logger.debug(.init(stringLiteral: res.0.logValue))
             throw error
         }
     }
-    
+
     ///
     /// Uploads the contents of the request and returns the response
     ///
@@ -99,19 +93,19 @@ public struct UrlSessionHttpClient: HttpClient {
         guard let data = urlRequest.httpBody else {
             throw HttpError.missingUploadData
         }
-        
+
         logger.info(.init(stringLiteral: urlRequest.curlString))
-        
+
         // Type: (Data, URLResponse)
         let res = try await session.upload(
             for: urlRequest,
             from: data,
             delegate: nil
         )
-        
+
         do {
             let rawResponse = try HttpRawResponse(res)
-            // logger.trace(.init(stringLiteral: rawResponse.traceLogValue))
+            logger.trace(.init(stringLiteral: rawResponse.traceLogValue))
             logger.debug(.init(stringLiteral: res.0.logValue))
             return rawResponse
         } catch {
@@ -119,7 +113,7 @@ public struct UrlSessionHttpClient: HttpClient {
             throw error
         }
     }
-    
+
     ///
     /// Downloads the contents of the request and returns the response
     ///
@@ -132,17 +126,17 @@ public struct UrlSessionHttpClient: HttpClient {
     public func downloadTask(_ req: HttpRequest) async throws -> HttpResponse {
         let urlRequest = req.urlRequest
         logger.info(.init(stringLiteral: urlRequest.curlString))
-        
+
         // Type: (URL, URLResponse)
         let res = try await session.download(for: urlRequest, delegate: nil)
-        
+
         guard let pathData = res.0.path.data(using: .utf8) else {
             throw HttpError.invalidResponse
         }
-        
+
         do {
             let rawResponse = try HttpRawResponse((pathData, res.1))
-            // logger.trace(.init(stringLiteral: rawResponse.traceLogValue))
+            logger.trace(.init(stringLiteral: rawResponse.traceLogValue))
             logger.debug(.init(stringLiteral: res.0.absoluteString))
             return rawResponse
         }
